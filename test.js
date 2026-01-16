de32cb2398b54840e95ac9972e73476de9505a1f
nerddude <30808467+nerddude24@users.noreply.github.com>
2026-01-16 15:25:52 +0100

    improve js test

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
-function scalarMulFalse(mat, scalar) {
+function scalarMulStupid(mat, scalar) {
 	const result = mat;
 
-	// This order causes significantly more cache misses in C, let's see if it's the same in JS.
 	for (let x = 0; x < mat[0].length; x++) {
 		for (let y = 0; y < mat.length; y++) {
 			result[y][x] *= scalar;
@@ -23,15 +22,23 @@ function scalarMulCorrect(mat, scalar) {
 	return result;
 }
 
-function scalarMulEvenFaster(mat, scalar) {
+function scalarMulCorrect4x(mat, scalar) {
 	const result = mat;
 
 	for (let y = 0; y < mat.length; y++) {
 		for (let x = 0; x < mat[0].length; x += 4) {
 			result[y][x] *= scalar;
-			result[y][x + 1] *= scalar;
-			result[y][x + 2] *= scalar;
-			result[y][x + 3] *= scalar;
+
+			if (x + 3 >= mat[0].length) {
+				// clean leftovers incase MATRIX_SIZE % 4 != 0
+				for (let leftoverX = x + 1; leftoverX < mat[0].length; leftoverX++)
+					result[y][leftoverX] *= scalar;
+				break;
+			} else {
+				result[y][x + 1] *= scalar;
+				result[y][x + 2] *= scalar;
+				result[y][x + 3] *= scalar;
+			}
 		}
 	}
 
@@ -44,13 +51,13 @@ function test() {
 		Array.apply(null, Array(MATRIX_SIZE)).map(_ => 1)
 	);
 
-	const SCALAR = 10. // it's value shouldn't matter.
+	const SCALAR = 10 // value shouldn't matter.
 
-	// NOTE: these timing are on my machine
+	// NOTE: these timing are on my machine on MATRIX_SIZE 10000 using node
 
 	// 1x faster (baseline)
 	console.time("stupid loop order");
-	scalarMulFalse(matrix, SCALAR);
+	scalarMulStupid(matrix, SCALAR);
 	console.timeEnd("stupid loop order");
 
 
@@ -59,9 +66,9 @@ function test() {
 	scalarMulCorrect(matrix, SCALAR);
 	console.timeEnd("correct loop order");
 
-	// 20x~ faster
+	// 17x~ faster (20x~ without out-of-bounds check)
 	console.time("correct loop order + 4 at a time");
-	scalarMulEvenFaster(matrix, SCALAR);
+	scalarMulCorrect4x(matrix, SCALAR);
 	console.timeEnd("correct loop order + 4 at a time");
 }
 
